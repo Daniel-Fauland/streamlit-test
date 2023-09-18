6e280791f5996a91258fe8d96c3c30d9e6932fd2
DanielFauland <looser447@gmail.com>
2023-09-18 18:35:15 +0200

    Update streamlit app

--- app.py
+++ app.py
@@ -1,138 +1,9 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import cv2
-import openai
-import wikipediaapi
-import pdfplumber
-import pytesseract
-from pdf2image import convert_from_bytes
+import time
+import backend
 
 
-def get_wikipedia_content(url):
-    if "de.wikipedia.org" in url:
-        country_flag = "de"
-    else:
-        country_flag = "en"
-    wiki = wikipediaapi.Wikipedia(
-        "Api-User-Agent", country_flag
-    ) 
-    page_name = url.split("/")[-1]  # Extract the page name from the URL
-    page = wiki.page(page_name)
-
-    if page.exists():
-        return page.text
-    else:
-        return "Page not found."
-
-
-def ask_gpt(kwargs):
-    print("Inside GPT func")
-    simplify = ""
-    if kwargs["simplify"] == "yes":
-        simplify = ". Explain it in a way so that a child can understand it. "
-    if kwargs["language"] == "German":
-        prompt = f"Translate the following text to german while also giving a {kwargs['length']} summary of the translation{simplify}Only return the summarized translation:\n{kwargs['prompt']}"
-    if kwargs["language"] == "English":
-        prompt = f"Give a {kwargs['length']} summary of the following text{simplify}:\n{kwargs['prompt']}"
-    openai.api_key = api_key
-
-    prompt = prompt.strip()
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k", messages=[{"role": "user", "content": prompt}]
-    )
-    return completion
-
-
-def read_pdf(file):
-    pdf_lang = st.radio("Choose the language of your pdf document", ["German", "English"])
-    with pdfplumber.open(file) as pdf:
-        page_count = len(pdf.pages)
-        if page_count == 1:
-            st.write(f"There is 1 page in your pdf document")
-        elif page_count > 1:
-            st.write(f"There are {len(pdf.pages)} pages in your pdf document")
-        else:
-            st.write(f"No pages found in your pdf document. Please check your uploaded file!")
-            return None
-        
-        pdf_start_page = st.number_input(f"Choose your start page between 1 and {page_count}", 1, page_count, value=1)
-        if pdf_start_page:
-            pdf_end_page = st.number_input(f"Choose your end page between {pdf_start_page} and {page_count}", pdf_start_page, page_count, value=pdf_start_page)
-        if st.button("Start"):
-            content = extract_pdf_text(pdf, int(pdf_start_page), int(pdf_end_page), file, pdf_lang)
-            handle_button_click(content)
-
-
-def extract_pdf_text(pdf, start, end, file, pdf_lang):
-    content = ""
-    for i in range(len(pdf.pages)):
-            if i+1 >= start and i+1 <= end:
-                page = pdf.pages[i]
-                content += page.extract_text()
-                content += "\n\n" 
-    if len(content) < 10:
-        st.write("There is no selectable text in the pdf document. Switching to OCR mode...")
-        content = ocr_text_from_file(file, start, end, pdf_lang)
-        return content
-    else:
-        return content
-    
-
-def ocr_text_from_file(file, start, end, lang="deu"):
-    if lang == "English":
-        lang = "eng"
-    else: 
-        lang = "deu"
-    content = ""
-    file = file.getvalue()
-    pdf = convert_from_bytes(file)
-    for (i,page) in enumerate(pdf):
-        if i+1 >= start and i+1 <= end:
-            # transfer image of pdf_file into array
-            img = np.asarray(page)
-            # transfer into grayscale
-            cv2_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            st.image(cv2_img)
-            thresh = cv2.threshold(cv2_img, 150, 255, cv2.THRESH_BINARY_INV)[1]
-            result = cv2.GaussianBlur(thresh, (5,5), 0)
-            result = 255 - result
-            content += pytesseract.image_to_string(result, lang=lang, config='--psm 6')
-    return content
-
-
-def handle_button_click(input):
-    if len(input) > 3:
-        result = ask_gpt(
-            {
-                "prompt": input,
-                "length": length,
-                "simplify": simplify,
-                "language": language,
-            }
-        )
-        st.markdown("---")
-        result_msg = result.choices[0].message.content
-        result_prompt_tokens = result.usage.prompt_tokens
-        result_output_tokens = result.usage.completion_tokens
-        result_tokens = result_prompt_tokens + result_output_tokens
-        cost = (result_prompt_tokens / 1000) * 0.003 + (
-            result_output_tokens / 1000
-        ) * 0.004
-        output_msg = st.text_area("GPT result", value=result_msg, height=350)
-        output_tokens = st.text(f"Tokens used for completing the task: {result_tokens}")
-        output_price = st.text(f"Cost for completing the task: {round(cost, 4)}$")
-        st.markdown("---")
-    st.session_state.prompt = ""
-
-
-def handle_wiki_click():
-    url = prompt
-    content = get_wikipedia_content(url)
-    if len(content) > 10:
-        handle_button_click(content)
-
-def user_options():
+def user_options_summarize(mode):
     length = st.select_slider(
         "Choose the length of the summary",
         options=["Very short", "short", "detailed", "very detailed"],
@@ -142,8 +13,65 @@ def user_options():
         "Simplify the output to make it more accessible for children",
         options=("No", "Yes"),
     )
+    if mode == "PDF Upload":
+        inp_language = st.radio("Select Input language", options=("German", "English"))
+    else:
+        inp_language = ""
+    language = ""
+    return length, simplify, inp_language, language
+
+
+def user_options_translate(mode):
+    length = ""
+    simplify = ""
+    if mode == "PDF Upload":
+        inp_language = st.radio("Select Input language", options=("German", "English"))
+    else:
+        inp_language = ""
     language = st.radio("Select Output language", options=("German", "English"))
-    return length, simplify, language
+    return length, simplify, inp_language, language
+
+
+def user_options_summarize_translate(mode):
+    length = st.select_slider(
+        "Choose the length of the summary",
+        options=["Very short", "short", "detailed", "very detailed"],
+        value=("short"),
+    )
+    simplify = st.radio(
+        "Simplify the output to make it more accessible for children",
+        options=("No", "Yes"),
+    )
+    if mode == "PDF Upload":
+        inp_language = st.radio("Select Input language", options=("German", "English"))
+    else:
+        inp_language = ""
+    language = st.radio("Select Output language", options=("German", "English"))
+    return length, simplify, inp_language, language
+
+
+def mode_options(user_option):
+        if user_option == "summarize":
+            length, simplify, inp_language, language = user_options_summarize(mode)
+        elif user_option == "translate":
+            length, simplify, inp_language, language = user_options_translate(mode)
+        elif user_option == "summarize_translate":
+            length, simplify, inp_language, language = user_options_summarize_translate(mode)
+        if mode == "Manual input":
+            prompt = st.text_area("Enter any text to summarize", value="", height=150)
+            if st.button("Start"):
+                start_time = time.time()
+                backend.handle_button_click(feature, api_key, start_time, prompt, length, simplify, language, inp_language)
+        elif mode == "Wikipedia URL":
+            prompt = st.text_input("Enter any wikipedia URL", value="")
+            if st.button("Start"):
+                start_time = time.time()
+                backend.handle_wiki_click(feature, api_key, start_time, prompt, length, simplify, language, inp_language)
+        elif mode == "PDF Upload":
+            file = st.file_uploader("Upload a PDF file", type="pdf", accept_multiple_files=False)
+            if file:
+                start_time = time.time()
+                backend.read_pdf(feature, api_key, start_time, file, length, simplify, language, inp_language)
 
 
 st.set_page_config(page_title="GPT-Summarizer")
@@ -169,23 +97,17 @@ if api_key:
     input_placeholder.empty()
     st.title("Summarize and translate your texts using GPT 3.5")
 
+    feature = st.selectbox(
+        "Choose your preferred feature", options=("Summarize only", "Translate only", "Summarize & Translate")
+    )
+
     mode = st.selectbox(
         "Choose your preferred way of input", options=("Manual input", "Wikipedia URL", "PDF Upload")
     )
 
-    if mode == "Manual input":
-        length, simplify, language = user_options()
-        prompt = st.text_area("Enter any text to summarize", value="", height=150)
-        if st.button("Start"):
-            handle_button_click(prompt)
-    elif mode == "Wikipedia URL":
-        length, simplify, language = user_options()
-        prompt = st.text_input("Enter any wikipedia URL", value="")
-        if st.button("Start"):
-            handle_wiki_click()
-    elif mode == "PDF Upload":
-        file = st.file_uploader("Upload a PDF file", type="pdf", accept_multiple_files=False)
-        length, simplify, language = user_options()
-        if file:
-            read_pdf(file)
-            
+    if feature == "Summarize only":
+        mode_options("summarize")
+    elif feature == "Translate only":
+        mode_options("translate")
+    elif feature == "Summarize & Translate":
+        mode_options("summarize_translate")
---
+++ backend.py
@@ -0,0 +1,185 @@
+import streamlit as st
+import langid
+import time
+import numpy as np
+import pandas as pd
+import cv2
+import openai
+import wikipediaapi
+import pdfplumber
+import pytesseract
+from pdf2image import convert_from_bytes
+
+
+def measure_response_time(start_time, text):
+    now = time.time()
+    diff = now - start_time
+    text = f"Duration for {text}: {diff:.2f}"
+    return text
+
+
+def detect_language(text):
+    lang, _ = langid.classify(text)
+    if lang == "de":
+        inp_language = "German"
+    else:
+        inp_language = "English"
+    print(f"Detected language: {inp_language}")
+    return inp_language
+
+
+def get_wikipedia_content(url):
+    if "de.wikipedia.org" in url:
+        country_flag = "de"
+        inp_language = "German"
+    else:
+        country_flag = "en"
+        inp_language = "English"
+    wiki = wikipediaapi.Wikipedia(
+        "Api-User-Agent", country_flag
+    ) 
+    page_name = url.split("/")[-1]  # Extract the page name from the URL
+    page = wiki.page(page_name)
+
+    if page.exists():
+        return page.text, inp_language
+    else:
+        return "Page not found.", inp_language
+
+
+def ask_gpt(api_key, kwargs):
+    openai.api_key = api_key
+    simplify = ". "
+    if kwargs["simplify"] == "yes":
+        simplify = ". Explain it in a way so that a child can understand it. "
+
+    feature = kwargs["feature"]
+    if feature == "Summarize only":
+        if kwargs["inp_language"] == "German":
+            prompt = f"Give a {kwargs['length']} summary of the following text in german{simplify}:\n{kwargs['prompt']}"
+        if kwargs["inp_language"] == "English":
+            prompt = f"Give a {kwargs['length']} summary of the following text{simplify}:\n{kwargs['prompt']}"
+
+    elif feature == "Translate only":
+        if kwargs["inp_language"] == "German":
+            prompt = f"Translate the following text to english and only return the translation:\n{kwargs['prompt']}"
+        if kwargs["inp_language"] == "English":
+            prompt = f"Translate the following text to german and only return the translation:\n{kwargs['prompt']}" 
+
+    else: 
+        if kwargs["inp_language"] == "German":
+            if kwargs["language"] == "German":
+                prompt = f"Give a {kwargs['length']} summary of the following text in german{simplify}:\n{kwargs['prompt']}"
+            if kwargs["language"] == "English":
+                prompt = f"Translate the following text to english while also giving a {kwargs['length']} summary of the translation{simplify}Only return the summarized translation:\n{kwargs['prompt']}"
+        if kwargs["inp_language"] == "English":
+            if kwargs["language"] == "German":
+                prompt = f"Translate the following text to german while also giving a {kwargs['length']} summary of the translation{simplify}Only return the summarized translation:\n{kwargs['prompt']}"
+            if kwargs["language"] == "English":
+                prompt = f"Give a {kwargs['length']} summary of the following text{simplify}:\n{kwargs['prompt']}"
+
+
+    prompt = prompt.strip()
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k", messages=[{"role": "user", "content": prompt}]
+    )
+    return completion
+
+
+def read_pdf(feature, api_key, start_time, file, length, simplify, language, inp_language):
+    pdf_lang = st.radio("Choose the language of your pdf document", ["German", "English"])
+    with pdfplumber.open(file) as pdf:
+        page_count = len(pdf.pages)
+        if page_count == 1:
+            st.write(f"There is 1 page in your pdf document")
+        elif page_count > 1:
+            st.write(f"There are {len(pdf.pages)} pages in your pdf document")
+        else:
+            st.write(f"No pages found in your pdf document. Please check your uploaded file!")
+            return None
+        
+        pdf_start_page = st.number_input(f"Choose your start page between 1 and {page_count}", 1, page_count, value=1)
+        if pdf_start_page:
+            pdf_end_page = st.number_input(f"Choose your end page between {pdf_start_page} and {page_count}", pdf_start_page, page_count, value=pdf_start_page)
+        if st.button("Start"):
+            content = extract_pdf_text(pdf, int(pdf_start_page), int(pdf_end_page), file, pdf_lang)
+            handle_button_click(feature, api_key, start_time, content, length, simplify, language)
+
+
+def extract_pdf_text(pdf, start, end, file, pdf_lang):
+    content = ""
+    for i in range(len(pdf.pages)):
+            if i+1 >= start and i+1 <= end:
+                page = pdf.pages[i]
+                content += page.extract_text()
+                content += "\n\n" 
+    if len(content) < 10:
+        st.write("There is no selectable text in the pdf document. Switching to OCR mode...")
+        content = ocr_text_from_file(file, start, end, pdf_lang)
+        return content
+    else:
+        return content
+    
+
+def ocr_text_from_file(file, start, end, lang="deu"):
+    if lang == "English":
+        lang = "eng"
+    else: 
+        lang = "deu"
+    content = ""
+    file = file.getvalue()
+    pdf = convert_from_bytes(file)
+    for (i,page) in enumerate(pdf):
+        if i+1 >= start and i+1 <= end:
+            now = time.time()
+            # transfer image of pdf_file into array
+            img = np.asarray(page)
+            # transfer into grayscale
+            cv2_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            st.image(cv2_img, width=250)
+            thresh = cv2.threshold(cv2_img, 150, 255, cv2.THRESH_BINARY_INV)[1]
+            result = cv2.GaussianBlur(thresh, (5,5), 0)
+            result = 255 - result
+            st.text(measure_response_time(now, f"reading CV2 image {i+1}/{len(pdf)}"))
+            now = time.time()
+            content += pytesseract.image_to_string(result, lang=lang, config='--psm 6')
+            st.text(measure_response_time(now, f"OCR image {i+1}/{len(pdf)}"))
+    return content
+
+def handle_button_click(feature, api_key, start_time, input, length, simplify, language, inp_language):
+    if len(input) > 3:
+        if inp_language == "":
+            inp_language = detect_language(input)
+        now = time.time()
+        result = ask_gpt(api_key,
+            {
+                "prompt": input,
+                "length": length,
+                "simplify": simplify,
+                "language": language,
+                "inp_language": inp_language,
+                "feature": feature
+            }
+        )
+        st.markdown("---")
+        result_msg = result.choices[0].message.content
+        result_prompt_tokens = result.usage.prompt_tokens
+        result_output_tokens = result.usage.completion_tokens
+        result_tokens = result_prompt_tokens + result_output_tokens
+        cost = (result_prompt_tokens / 1000) * 0.003 + (
+            result_output_tokens / 1000
+        ) * 0.004
+        output_msg = st.text_area("GPT result", value=result_msg, height=350)
+        output_tokens = st.text(f"Tokens used for completing the task: {result_tokens}")
+        output_price = st.text(f"Cost for completing the task: {round(cost, 4)}$")
+        api_duration = st.text(measure_response_time(now, "GPT API call"))
+        full_duration = st.text(measure_response_time(start_time, "entire response"))
+        st.markdown("---")
+    st.session_state.prompt = ""
+
+
+def handle_wiki_click(feature, api_key, start_time, input, length, simplify, language, inp_language):
+    content, inp_language = get_wikipedia_content(input)
+
+    if len(content) > 10:
+        handle_button_click(feature, api_key, start_time, content, length, simplify, language, inp_language)
